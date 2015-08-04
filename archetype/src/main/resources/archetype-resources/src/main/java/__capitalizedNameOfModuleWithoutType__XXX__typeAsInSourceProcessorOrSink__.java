3445b1029bf2f55d6c09e220bab34de8945de518
Eric Bottard <eric.bottard+github@gmail.com>
2015-08-04 18:56:56 +0200

    Provide a maven archetype

---
+++ archetype/src/main/resources/archetype-resources/src/main/java/__capitalizedNameOfModuleWithoutType__XXX__typeAsInSourceProcessorOrSink__.java
@@ -0,0 +1,41 @@
+#set( $symbol_pound = '#' )
+#set( $symbol_dollar = '$' )
+#set( $symbol_escape = '\' )
+/*
+ * Copyright 2015 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package ${package};
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.cloud.stream.annotation.EnableModule;
+import org.springframework.cloud.stream.annotation.${typeAsInSourceProcessorOrSink};
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+
+/**
+ * This is the main module class.
+ */
+@EnableModule(${typeAsInSourceProcessorOrSink}.class)
+@EnableConfigurationProperties(${capitalizedNameOfModuleWithoutType}XXX${typeAsInSourceProcessorOrSink}Options.class)
+// The XXX part is needed to workaround a maven archetype bug. You'll want to perform a rename refactoring.
+public class ${capitalizedNameOfModuleWithoutType}XXX${typeAsInSourceProcessorOrSink} {
+
+	private static final Logger logger = LoggerFactory.getLogger(${capitalizedNameOfModuleWithoutType}XXX${typeAsInSourceProcessorOrSink}.class);
+
+	@Autowired
+	private ${capitalizedNameOfModuleWithoutType}XXX${typeAsInSourceProcessorOrSink}Options options;
+}
---
+++ archetype/src/main/resources/archetype-resources/src/main/java/__capitalizedNameOfModuleWithoutType__XXX__typeAsInSourceProcessorOrSink__Options.java
@@ -0,0 +1,32 @@
+#set( $symbol_pound = '#' )
+#set( $symbol_dollar = '$' )
+#set( $symbol_escape = '\' )
+/*
+ * Copyright 2015 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package ${package};
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+/**
+ * This is the main module class.
+ */
+@ConfigurationProperties
+// The XXX part is needed to workaround a maven archetype bug. You'll want to perform a rename refactoring.
+public class ${capitalizedNameOfModuleWithoutType}XXX${typeAsInSourceProcessorOrSink}Options {
+
+
+}
