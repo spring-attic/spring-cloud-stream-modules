1e4ae1e0f8b1bf0e912ce8fc4e0cd8f7d8a16804
David Turanski <dturanski@pivotal.io>
2015-08-21 13:44:45 -0400

    XD-3376 Gemfire Sink

---
+++ gemfire-sink/src/main/java/org/springframework/cloud/stream/module/gemfire/GemfireSink.java
@@ -0,0 +1,60 @@
+/*
+ * Copyright 2015 the original author or authors.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.springframework.cloud.stream.module.gemfire;
+
+import java.util.Collections;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.Resource;
+
+import com.gemstone.gemfire.cache.Region;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.cloud.stream.annotation.EnableModule;
+import org.springframework.cloud.stream.annotation.ModuleChannels;
+import org.springframework.cloud.stream.annotation.Sink;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Import;
+import org.springframework.integration.gemfire.outbound.CacheWritingMessageHandler;
+
+/**
+ * @author David Turanski
+ */
+@EnableModule(Sink.class)
+@Import({GemfirePoolConfiguration.class, GemfireClientRegionConfiguration.class})
+@EnableConfigurationProperties(GemfireSinkProperties.class)
+public class GemfireSink {
+	@Autowired
+	GemfireSinkProperties config;
+
+	@Autowired
+	ApplicationContext context;
+	
+	@Resource(name = "clientRegion")
+	Region<String,?> region;
+
+	@Autowired
+	@ModuleChannels(GemfireSink.class)
+	private Sink gemfireSink;
+
+	@PostConstruct
+	public void init() {
+		CacheWritingMessageHandler messageHandler = new CacheWritingMessageHandler(region);
+		messageHandler.setCacheEntries(Collections.singletonMap(config.getKeyExpression(), "payload"));
+		gemfireSink.input().subscribe(messageHandler);
+	}
+}
