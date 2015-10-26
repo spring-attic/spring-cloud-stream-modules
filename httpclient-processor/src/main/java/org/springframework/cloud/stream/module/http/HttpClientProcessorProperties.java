7c71998c2f32944b7b5f61222b05c83a8d7d4de5
Waldemar Hummer <hummer@infosys.tuwien.ac.at>
2015-10-26 17:38:58 +0100

    add SpEL expression support for URL, headers, and request body; allow configuration of expectedReturnType; extend tests

--- httpclient-processor/src/main/java/org/springframework/cloud/stream/module/http/HttpClientProcessorProperties.java
+++ httpclient-processor/src/main/java/org/springframework/cloud/stream/module/http/HttpClientProcessorProperties.java
@@ -1,5 +1,7 @@
 package org.springframework.cloud.stream.module.http;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.expression.Expression;
+import org.springframework.expression.spel.standard.SpelExpressionParser;
 
 /**
  * Configuration properties for the Http Client Processor module.
@@ -11,16 +13,22 @@ public class HttpClientProcessorProperties {
 
 	private static final String DEFAULT_HTTP_METHOD = "GET";
 
-	private String url;
+	private static final Expression DEFAULT_RETURN_TYPE = new SpelExpressionParser().parseExpression("''.class");
+
+	private Expression url;
 
 	private String httpMethod = DEFAULT_HTTP_METHOD;
 
-	private Object body;
+	private Expression body;
 
-	public String getUrl() {
+	private Expression headers;
+
+	private Expression expectedReturnType = DEFAULT_RETURN_TYPE;
+
+	public Expression getUrl() {
 		return url;
 	}
-	public void setUrl(String url) {
+	public void setUrl(Expression url) {
 		this.url = url;
 	}
 
@@ -31,11 +39,25 @@ public class HttpClientProcessorProperties {
 		this.httpMethod = httpMethod;
 	}
 
-	public Object getBody() {
+	public Expression getBody() {
 		return body;
 	}
-	public void setBody(Object body) {
+	public void setBody(Expression body) {
 		this.body = body;
+	}
+
+	public Expression getHeaders() {
+		return headers;
+	}
+	public void setHeaders(Expression headers) {
+		this.headers = headers;
+	}
+
+	public Expression getExpectedReturnType() {
+		return expectedReturnType;
+	}
+	public void setExpectedReturnType(Expression expectedReturnType) {
+		this.expectedReturnType = expectedReturnType;
 	}
 
 }
