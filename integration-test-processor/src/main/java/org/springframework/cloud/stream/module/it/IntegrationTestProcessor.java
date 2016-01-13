f0a3cb8de7ad7ec17e01583752f9892ea72c7418
Eric Bottard <eric.bottard+github@gmail.com>
2016-01-13 14:28:26 +0100

    Add module used in integration tests
    Change the way instance index is retrieved

---
+++ integration-test-processor/src/main/java/org/springframework/cloud/stream/module/it/IntegrationTestProcessor.java
@@ -0,0 +1,68 @@
+/*
+ * Copyright 2016 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.springframework.cloud.stream.module.it;
+
+import javax.annotation.PostConstruct;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.cloud.stream.annotation.EnableBinding;
+import org.springframework.cloud.stream.config.ChannelBindingServiceProperties;
+import org.springframework.cloud.stream.messaging.Processor;
+
+/**
+ * A module that can misbehave, useful for integration testing of module deployers.
+ *
+ * @author Eric Bottard
+ */
+@EnableBinding(Processor.class)
+@EnableConfigurationProperties(IntegrationTestProcessorProperties.class)
+public class IntegrationTestProcessor {
+
+	@Autowired
+	private IntegrationTestProcessorProperties properties;
+
+	@Autowired
+	private ChannelBindingServiceProperties channelBindingServiceProperties;
+
+	@PostConstruct
+	public void init() throws InterruptedException {
+		if (properties.getMatchInstances().isEmpty() || properties.getMatchInstances().contains(instanceIndex())) {
+			Thread.sleep(properties.getInitDelay());
+			if (properties.getKillDelay() >= 0) {
+				new Thread() {
+
+					@Override
+					public void run() {
+						try {
+							Thread.sleep(properties.getKillDelay());
+							System.exit(1);
+						}
+						catch (InterruptedException e) {
+							throw new RuntimeException(e);
+						}
+					}
+				}.start();
+			}
+		}
+	}
+
+	private int instanceIndex() {
+		return channelBindingServiceProperties.getInstanceIndex();
+	}
+
+}
---
+++ integration-test-processor/src/main/java/org/springframework/cloud/stream/module/it/IntegrationTestProcessorProperties.java
@@ -0,0 +1,73 @@
+/*
+ * Copyright 2016 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.springframework.cloud.stream.module.it;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+/**
+ * Configuration properties for the IntegrationTestProcessor module.
+ *
+ * @author Eric Bottard
+ */
+@ConfigurationProperties
+public class IntegrationTestProcessorProperties {
+
+	/**
+	 * The delay in milliseconds to stall the initialization of this module.
+	 * Useful for testing the 'deploying' state of a module.
+	 */
+	private int initDelay = 0;
+
+	/**
+	 * The delay in milliseconds after which this module will kill itself.
+	 * <p>-1 means don't kill</p>
+	 */
+	private int killDelay = -1;
+
+	/**
+	 * If not empty, only the module intances whose number(s) are contained in this set
+	 * will behave according to the other configuration parameters.
+	 */
+	private Set<Integer> matchInstances = new HashSet<>();
+
+	public int getInitDelay() {
+		return initDelay;
+	}
+
+	public void setInitDelay(int initDelay) {
+		this.initDelay = initDelay;
+	}
+
+	public int getKillDelay() {
+		return killDelay;
+	}
+
+	public void setKillDelay(int killDelay) {
+		this.killDelay = killDelay;
+	}
+
+	public Set<Integer> getMatchInstances() {
+		return matchInstances;
+	}
+
+	public void setMatchInstances(Set<Integer> matchInstances) {
+		this.matchInstances = matchInstances;
+	}
+}
