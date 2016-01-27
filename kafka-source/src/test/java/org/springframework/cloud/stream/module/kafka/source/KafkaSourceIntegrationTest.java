75748f1dbd3dd05cd622e58648e66647377bdfd6
Soby Chacko <schacko@pivotal.io>
2016-01-27 11:17:45 -0500

    simplifying the Kafka source integrtaiton test producer

--- kafka-source/src/test/java/org/springframework/cloud/stream/module/kafka/source/KafkaSourceIntegrationTest.java
+++ kafka-source/src/test/java/org/springframework/cloud/stream/module/kafka/source/KafkaSourceIntegrationTest.java
@@ -18,17 +18,15 @@
 
 package org.springframework.cloud.stream.module.kafka.source;
 
-import com.gs.collections.api.block.function.Function0;
-import com.gs.collections.impl.list.mutable.FastList;
 import kafka.admin.AdminUtils;
-import kafka.serializer.Encoder;
-import org.apache.kafka.common.serialization.StringSerializer;
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.Producer;
+import org.apache.kafka.clients.producer.ProducerRecord;
 import org.junit.ClassRule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.InitializingBean;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.test.IntegrationTest;
 import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.cloud.stream.messaging.Source;
@@ -36,27 +34,10 @@ import org.springframework.cloud.stream.test.binder.MessageCollector;
 import org.springframework.cloud.stream.test.junit.kafka.KafkaTestSupport;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.integration.IntegrationMessageHeaderAccessor;
 import org.springframework.integration.config.EnableIntegration;
-import org.springframework.integration.dsl.GenericEndpointSpec;
-import org.springframework.integration.dsl.IntegrationFlow;
-import org.springframework.integration.dsl.IntegrationFlowDefinition;
-import org.springframework.integration.dsl.PublishSubscribeSpec;
-import org.springframework.integration.dsl.kafka.Kafka;
-import org.springframework.integration.dsl.kafka.KafkaProducerMessageHandlerSpec;
-import org.springframework.integration.dsl.support.Consumer;
-import org.springframework.integration.dsl.support.Function;
-import org.springframework.integration.dsl.support.PropertiesBuilder;
-import org.springframework.integration.expression.ValueExpression;
-import org.springframework.integration.kafka.outbound.KafkaProducerMessageHandler;
-import org.springframework.integration.kafka.serializer.common.StringEncoder;
 import org.springframework.integration.kafka.support.KafkaHeaders;
-import org.springframework.integration.kafka.support.ProducerMetadata;
-import org.springframework.integration.kafka.util.EncoderAdaptingSerializer;
 import org.springframework.integration.kafka.util.TopicUtils;
 import org.springframework.messaging.Message;
-import org.springframework.messaging.MessageChannel;
-import org.springframework.messaging.support.GenericMessage;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
@@ -88,27 +69,27 @@ public class KafkaSourceIntegrationTest {
     protected MessageCollector messageCollector;
 
     @Autowired
-    @Qualifier("sendToKafkaFlow.input")
-    protected MessageChannel sendToKafkaFlowInput;
-
-    @Autowired
-    @Qualifier("kafkaProducer.handler")
-    protected KafkaProducerMessageHandler kafkaProducer;
-
-    @Autowired
     protected Source source;
+
+    @Autowired
+    protected Producer<String, String> producer;
 
     @Test
     public void testBasicSource() throws Exception {
-        this.kafkaProducer.setPartitionIdExpression(new ValueExpression<>(0));
 
-        this.sendToKafkaFlowInput.send(new GenericMessage<>("foo"));
+        for (int i = 0; i < 100; i++) {
+
+            producer.send(new ProducerRecord<>("test-topic",
+                    "test-topic" + i,
+                    Integer.toString(i)));
+        }
+        producer.close();
 
         for (int i = 0; i < 100; i++) {
             Message<?> out = this.messageCollector.forChannel(this.source.output()).poll(10, TimeUnit.SECONDS);
             assertNotNull(out);
             String payload = new String(((byte[]) out.getPayload()), "UTF-8");
-            assertEquals("foo", payload);
+            assertEquals(Integer.toString(i), payload);
             assertEquals(TEST_TOPIC, out.getHeaders().get(KafkaHeaders.TOPIC));
         }
     }
@@ -118,100 +99,45 @@ public class KafkaSourceIntegrationTest {
     public static class ContextConfiguration {
 
         @Bean
-        public IntegrationFlow sendToKafkaFlow() {
-            return new IntegrationFlow() {
+        public Producer<String, String> producer() {
+
+            Properties props = new Properties();
+            props.put("queue.buffering.max.ms", "15000");
+            props.put("bootstrap.servers", kafkaTestSupport.getBrokerAddress());
+            props.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+            props.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+            Producer<String, String> producer = new KafkaProducer<>(props);
+
+            return producer;
+        }
+
+        @Bean(destroyMethod = "destroy")
+        public InitializingBean topicManager() {
+            return new InitializingBean() {
+
+                private final List<String> topics = Arrays.asList(TEST_TOPIC);
+
                 @Override
-                public void configure(IntegrationFlowDefinition<?> f) {
-                    f.<String>split(new Function<Object, Object>() {
+                public void afterPropertiesSet() throws Exception {
+                    this.topics.forEach(new java.util.function.Consumer<String>() {
                         @Override
-                        public Object apply(final Object p) {
-                            return FastList.newWithNValues(100, new Function0<Object>() {
-                                @Override
-                                public Object value() {
-                                    return p;
-                                }
-                            });
+                        public void accept(String t) {
+                            TopicUtils.ensureTopicCreated(kafkaTestSupport.getZkConnectString(), t, 1, 1);
                         }
-                    }, null)
-                    .publishSubscribeChannel(new Consumer<PublishSubscribeSpec>() {
-                         @Override
-                         public void accept(PublishSubscribeSpec c) {
-                             c.subscribe(new IntegrationFlow() {
-                                 @Override
-                                 public void configure(IntegrationFlowDefinition<?> sf) {
-                                     sf.handle(kafkaMessageHandler(kafkaTestSupport.getBrokerAddress(), TEST_TOPIC),
-                                         new Consumer<GenericEndpointSpec<KafkaProducerMessageHandler>>() {
-                                             @Override
-                                             public void accept(GenericEndpointSpec<KafkaProducerMessageHandler> e) {
-                                                 e.id("kafkaProducer");
-                                             }
-                                         }
-                                     );
-                                 }
-                             });
-                         }
                     });
                 }
+
+                public void destroy() {
+                    this.topics.forEach(new java.util.function.Consumer<String>() {
+                        @Override
+                        public void accept(String t) {
+                            AdminUtils.deleteTopic(kafkaTestSupport.getZkClient(), t);
+                            //TestUtils.waitUntilMetadataIsPropagated(asScalaBuffer(), t, 0, 5000L);
+                        }
+                    });
+                }
+
             };
         }
-    }
-
-    @SuppressWarnings("unchecked")
-    private static KafkaProducerMessageHandlerSpec kafkaMessageHandler(String serverAddress, String topic) {
-        Encoder<?> stringEncoder = new StringEncoder();
-
-        Properties props = new Properties();
-        props.put("queue.buffering.max.ms", "15000");
-
-        return Kafka
-                .outboundChannelAdapter(new Consumer<PropertiesBuilder>() {
-                    @Override
-                    public void accept(PropertiesBuilder props) {
-                        props
-                                .put("queue.buffering.max.ms", "15000");
-                    }
-                })
-                .messageKey(new Function<Message<Object>, Object>() {
-                    @Override
-                    public Object apply(Message<Object> m) {
-                        return "test-topic" + m
-                                .getHeaders()
-                                .get(IntegrationMessageHeaderAccessor.SEQUENCE_NUMBER);
-                    }
-                })
-                .addProducer(
-                        new ProducerMetadata<>(topic, String.class, String.class,
-                                new EncoderAdaptingSerializer<>((Encoder<String>) stringEncoder),
-                                new StringSerializer()),
-                        serverAddress);
-    }
-
-    @Bean(destroyMethod = "destroy")
-    public InitializingBean topicManager() {
-        return new InitializingBean() {
-
-            private final List<String> topics = Arrays.asList(TEST_TOPIC);
-
-            @Override
-            public void afterPropertiesSet() throws Exception {
-                this.topics.forEach(new java.util.function.Consumer<String>() {
-                    @Override
-                    public void accept(String t) {
-                        TopicUtils.ensureTopicCreated(kafkaTestSupport.getZkConnectString(), t, 1, 1);
-                    }
-                });
-            }
-
-            public void destroy() {
-                this.topics.forEach(new java.util.function.Consumer<String>() {
-                    @Override
-                    public void accept(String t) {
-                        AdminUtils.deleteTopic(kafkaTestSupport.getZkClient(), t);
-                        //TestUtils.waitUntilMetadataIsPropagated(asScalaBuffer(), t, 0, 5000L);
-                    }
-                });
-            }
-
-        };
     }
 }
